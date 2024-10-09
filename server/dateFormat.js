67686788f65889380549bc21ddc54af48bb9e248
NivolasWang <320396130@qq.com>
2024-10-09 20:22:43 +0800

    Address_book

---
+++ server/dateFormat.js
@@ -0,0 +1,21 @@
+function dateFormat(dataStr) {
+  const dt = new Date(dataStr)
+
+  const y = dt.getFullYear()
+  const m = padZero(dt.getMonth() + 1)
+  const d = padZero(dt.getDate())
+
+  const hh = padZero(dt.getHours())
+  const mm = padZero(dt.getMinutes())
+  const ss = padZero(dt.getSeconds())
+
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+}
+
+function padZero(n) {
+  return n < 10 ? '0' + n : n
+}
+
+module.exports = {
+  dateFormat
+}
